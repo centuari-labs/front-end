f8f778688ebc10cfdd0009d0e7ceda67468b6831
yzxdano <ijulgrey@gmail.com>
2025-05-02 22:32:26 +0700

    change open order query

--- app/api/open-orders/[id]/[trader]/route.ts
+++ app/api/open-orders/[id]/[trader]/route.ts
@@ -9,7 +9,7 @@ export async function GET(
 
   const sql = neon(process.env.DATABASE_URL ?? "");
   const openOrders = await sql`
-  SELECT 
+SELECT 
   o.*, 
   lt.name as loan_token_name, lt.symbol as loan_token_symbol, lt.image_uri as loan_token_image_uri, lt.decimal as loan_token_decimal,
   ct.name as collateral_token_name, ct.symbol as collateral_token_symbol, ct.image_uri as collateral_token_image_uri, ct.decimal as collateral_token_decimal
@@ -17,7 +17,7 @@ FROM "order" o
   LEFT JOIN market m ON m.id = o.market_id
   LEFT JOIN token lt ON LOWER(lt.address) = LOWER(m.loan_token)
   LEFT JOIN token ct ON LOWER(ct.address) = LOWER(m.collateral_token)
-WHERE (status = 'OPEN' OR status = 'PARTIALLY_FILLED') AND trader = ${trader} AND market_id = ${id}
+WHERE (status = 'OPEN' OR status = 'PARTIALLY_FILLED') AND LOWER(o.trader) = LOWER(${trader}) AND LOWER(o.market_id) = LOWER(${id})
   `;
 
   const openOrderData = openOrders.map((openOrder) => ({

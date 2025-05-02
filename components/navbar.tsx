6e811419c94d11827e3b084622df4b40a035f2b1
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2025-05-02 22:40:28 +0700

    fix: update logo source path in Navbar component for consistent display

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -115,7 +115,7 @@ export function Navbar() {
             <Link href="/" className="flex items-center space-x-2">
               <div className="relative h-full w-40 rounded-full">
                 <img
-                  src={theme === "dark" ? "/logo-color.png" : "logo-black.png"}
+                  src={"logo-color.png"}
                   alt="DeFi Lending Logo"
                   className="h-full w-full object-cover"
                 />

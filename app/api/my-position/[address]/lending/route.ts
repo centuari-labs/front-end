d0bf17724f2450b0f1d66e7147b67cbd75225e50
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2025-05-03 23:01:52 +0700

    refactor: remove unused API routes for active borrow, lend, and vault

--- app/api/my-position/[address]/lending/route.ts
+++ app/api/my-position/[address]/lending/route.ts
@@ -9,15 +9,8 @@ export async function GET(
 
   const sql = neon(process.env.DATABASE_URL ?? "");
   const lending = await sql`
-  SELECT 
-  m.loan_token, lt.name as loan_token_name, lt.symbol as loan_token_symbol, lt.image_uri as loan_token_image_uri, lt.decimal as loan_token_decimal,
-  m.collateral_token, ct.name as collateral_token_name, ct.symbol as collateral_token_symbol, ct.image_uri as collateral_token_image_uri, ct.decimal as collateral_token_decimal,
-  m.centuari_token, cs.rate, m.maturity
-FROM pharos_centuari_supply cs
-  LEFT JOIN market m ON m.id = cs.market_id
-  LEFT JOIN token lt ON LOWER(lt.address) = LOWER(m.loan_token)
-  LEFT JOIN token ct ON LOWER(ct.address) = LOWER(m.collateral_token)
-WHERE cs.user = ${address}
+  SELECT * from active_lending_position
+  WHERE lower(trader) = lower(${address})
 `;
 
   return NextResponse.json(lending);
